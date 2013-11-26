876a41bab182625f52a19f1f7534318a8b0c293a
Tomohisa Takaoka <tomohisa@jtechs.com>
2013-11-26 22:24:41 +0900

    Added Options
    Made it adjustable dampering, frequency, distance and length to test
    out.

--- FourPointDynamicsSample/FourPointDynamicsSample/ViewController.m
+++ FourPointDynamicsSample/FourPointDynamicsSample/ViewController.m
@@ -13,6 +13,10 @@
 @property (weak, nonatomic) IBOutlet UIView *viewToMove;
 @property TOV4PointBehavior * fourPointBehavior;
 @property UIDynamicAnimator * animator;
+@property (weak, nonatomic) IBOutlet UILabel *labelDamping;
+@property (weak, nonatomic) IBOutlet UILabel *labelFrequency;
+@property (weak, nonatomic) IBOutlet UILabel *labelDistance;
+@property (weak, nonatomic) IBOutlet UILabel *labelLength;
 @end
 
 @implementation ViewController
@@ -22,7 +26,7 @@
     [super viewDidLoad];
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.fourPointBehavior = [[TOV4PointBehavior alloc] initWithItem:self.viewToMove attachedToAnchor:CGPointMake(100, 100) distance:100.];
+    self.fourPointBehavior = [[TOV4PointBehavior alloc] initWithItem:self.viewToMove attachedToAnchor:CGPointMake(100, 100) distance:200];
     [self.animator addBehavior:self.fourPointBehavior];
 
 }
@@ -34,6 +38,23 @@
 }
 - (IBAction)actionTapped:(UITapGestureRecognizer*)sender {
     self.fourPointBehavior.anchorPoint = [sender locationInView:self.view];
+}
+- (IBAction)valueChanged:(UISlider*)sender {
+    self.labelDamping.text = [NSString stringWithFormat:@"Daming (%f)",sender.value];
+    self.fourPointBehavior.damping = sender.value;
+
+}
+- (IBAction)valueChangedFrequency:(UISlider*)sender {
+    self.labelFrequency.text = [NSString stringWithFormat:@"Frequency (%f)",sender.value];
+    self.fourPointBehavior.frequency = sender.value;
+}
+- (IBAction)valueChangedDistance:(UISlider*)sender {
+    self.labelDistance.text = [NSString stringWithFormat:@"Distance (%f)",sender.value];
+    self.fourPointBehavior.distance = sender.value;
+}
+- (IBAction)valueChangedLength:(UISlider *)sender {
+    self.labelLength.text = [NSString stringWithFormat:@"Length (%f)",sender.value];
+    self.fourPointBehavior.length = sender.value;
 }
 
 @end
